372180f462f9a894997c2b77f379749822f507fe
Gonzalo Zelaya <gonzalo.zelaya@outsourcearg.com>
2024-10-29 17:07:13 -0300

    Updated model, still WIP

--- models/models.py
+++ models/models.py
@@ -1,39 +1,36 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api
-
+from odoo.exceptions import UserError
 class AllowSaleOrderLink(models.Model):
     _inherit = 'account.move'
     
     @api.onchange('invoice_origin')
     def _onchange_invoice_origin(self):
-        if self.invoice_origin:
-            # Buscar la orden de venta basada en invoice_origin
-            sale_order = self.env['sale.order'].search([('name', '=', self.invoice_origin)], limit=1)
-            if not sale_order:
-                raise UserError("No se encontró la orden de venta con el nombre especificado en invoice_origin.")
-            
-            # 1. Actualizar el conteo de facturas (invoice_count)
-            sale_order.invoice_count = len(sale_order.invoice_ids) + 1
+        for move in self:
+            if move.invoice_origin:
+                sale_order = self.env['sale.order'].search([('name', '=', move.invoice_origin)], limit=1)
+                if sale_order:
+                    for line in move.invoice_line_ids:
+                        sale_order_line = sale_order.order_line.filtered(
+                            lambda l: l.product_id.id == line.product_id.id
+                        )
+                        if sale_order_line:
+                            # Relacionar la línea de la factura con la línea de pedido correspondiente
+                            sale_order_line.invoice_lines.write([(4, sale_order_line.id)])
+                            #raise UserError(sale_order_line.invoice_lines)
+                            #sale_order_line.qty_invoiced += line.quantity
+                        else:
+                            # Crear una línea de pedido si no existe
+                            sale_order.order_line.create({
+                                'order_id': sale_order.id,
+                                'product_id': line.product_id.id,
+                                'qty_invoiced': line.quantity,
+                                'product_uom_qty': 0,
+                                'price_unit': line.price_unit,
+                                'name': line.name,
+                            })
 
-            # 2. Añadir la factura actual a las invoice_ids de la orden de venta
-            sale_order.write({'invoice_ids': [(4, self.id)]})
+                    # Forzar el recálculo para que se actualicen `invoice_count` y `invoice_ids`
+                    #sale_order._get_invoiced()
 
-            # 3. Actualizar la cantidad facturada (qty_invoiced) en las líneas de la orden de venta
-            for line in self.invoice_line_ids:
-                # Encontrar la línea de pedido correspondiente en la orden de venta
-                sale_order_line = sale_order.order_line.filtered(lambda l: l.product_id == line.product_id)
-
-                if sale_order_line:
-                    # Si existe, actualizar qty_invoiced sumando la cantidad de la línea de factura
-                    sale_order_line.qty_invoiced += line.quantity
-                else:
-                    # Si no existe una línea de pedido en la orden, crear una nueva con qty_invoiced
-                    sale_order.order_line.create({
-                        'order_id': sale_order.id,
-                        'product_id': line.product_id.id,
-                        'qty_invoiced': line.quantity,
-                        'product_uom_qty': 0,  # Cantidad solicitada puede quedar en 0 si solo es facturada
-                        'price_unit': line.price_unit,
-                        'name': line.name,
-                    })
